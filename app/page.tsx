417312cd2f8c65ccee2aba52cf98e89507fb9a71
Wayne Logan <loganamollo@gmail.com>
2024-10-02 21:43:02 +0300

    add images

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image"
+
 export default function Home() {
     return (
         <main className="
@@ -7,6 +9,34 @@ export default function Home() {
             bg-cover
             
         ">
+            <div className="
+                absolute
+                w-[200px]
+                h-[350px]
+                top-[10%]
+                mx-auto
+                left-1/2 transform -translate-x-1/2
+            ">
+                {
+                    Array(13).fill(0).map((index, key) => (
+                        <div key={index + key} className="
+                            absolute
+                            inset-0
+                        ">
+                            <Image
+                                src={`/img-${key + 1}.jpg`}
+                                width={200}
+                                height={250}
+                                alt="hashira"
+                                className="
+                                    w-full
+                                    h-full
+                                    object-cover
+                                "/>
+                        </div>
+                    ))
+                }
+            </div>
             <div>
                 <h1>鬼滅の刃</h1>
                 <h2>DEMON SLAYER</h2>
